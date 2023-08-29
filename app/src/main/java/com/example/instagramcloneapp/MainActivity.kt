c7f8bc5fd73adcea745504217f2da5cc3d579a72
Mohammed Shaat <mohammedshaat.it@gmail.com>
2023-08-29 13:11:16 +0300

    Commit 02

--- app/src/main/java/com/example/instagramcloneapp/MainActivity.kt
+++ app/src/main/java/com/example/instagramcloneapp/MainActivity.kt
@@ -1,5 +1,6 @@
 package com.example.instagramcloneapp
 
+import android.content.Intent
 import android.os.Bundle
 import com.google.android.material.bottomnavigation.BottomNavigationView
 import androidx.appcompat.app.AppCompatActivity
@@ -8,10 +9,37 @@ import androidx.navigation.ui.AppBarConfiguration
 import androidx.navigation.ui.setupActionBarWithNavController
 import androidx.navigation.ui.setupWithNavController
 import com.example.instagramcloneapp.databinding.ActivityMainBinding
+import com.google.android.material.navigation.NavigationBarView
 
 class MainActivity : AppCompatActivity() {
 
     private lateinit var binding: ActivityMainBinding
+
+    private val onItemSelectedListener = NavigationBarView.OnItemSelectedListener { item ->
+        when (item.itemId) {
+            R.id.nav_home -> {
+                return@OnItemSelectedListener true
+            }
+
+            R.id.nav_search -> {
+                return@OnItemSelectedListener true
+            }
+
+            R.id.nav_add_post -> {
+                return@OnItemSelectedListener true
+            }
+
+            R.id.nav_notifications -> {
+                return@OnItemSelectedListener true
+            }
+
+            R.id.nav_profile -> {
+                return@OnItemSelectedListener true
+            }
+        }
+
+        false
+    }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -21,15 +49,6 @@ class MainActivity : AppCompatActivity() {
 
         val navView: BottomNavigationView = binding.navView
 
-        val navController = findNavController(R.id.nav_host_fragment_activity_main)
-        // Passing each menu ID as a set of Ids because each
-        // menu should be considered as top level destinations.
-        val appBarConfiguration = AppBarConfiguration(
-            setOf(
-                R.id.navigation_home, R.id.navigation_dashboard, R.id.navigation_notifications
-            )
-        )
-        setupActionBarWithNavController(navController, appBarConfiguration)
-        navView.setupWithNavController(navController)
+        navView.setOnItemSelectedListener(onItemSelectedListener)
     }
 }

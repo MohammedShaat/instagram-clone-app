cb87aa8900571de9a6ef67e965d54b893f039df2
Mohammed Shaat <mohammedshaat.it@gmail.com>
2023-08-29 14:38:17 +0300

    Commit 03

--- app/src/main/java/com/example/instagramcloneapp/MainActivity.kt
+++ app/src/main/java/com/example/instagramcloneapp/MainActivity.kt
@@ -4,40 +4,52 @@ import android.content.Intent
 import android.os.Bundle
 import com.google.android.material.bottomnavigation.BottomNavigationView
 import androidx.appcompat.app.AppCompatActivity
+import androidx.fragment.app.Fragment
 import androidx.navigation.findNavController
 import androidx.navigation.ui.AppBarConfiguration
 import androidx.navigation.ui.setupActionBarWithNavController
 import androidx.navigation.ui.setupWithNavController
 import com.example.instagramcloneapp.databinding.ActivityMainBinding
+import com.example.instagramcloneapp.fragments.HomeFragment
+import com.example.instagramcloneapp.fragments.NotificationsFragment
+import com.example.instagramcloneapp.fragments.ProfileFragment
+import com.example.instagramcloneapp.fragments.SearchFragment
 import com.google.android.material.navigation.NavigationBarView
 
 class MainActivity : AppCompatActivity() {
 
     private lateinit var binding: ActivityMainBinding
 
+    internal var selectedFragment: Fragment? = null
+
     private val onItemSelectedListener = NavigationBarView.OnItemSelectedListener { item ->
         when (item.itemId) {
             R.id.nav_home -> {
-                return@OnItemSelectedListener true
+                selectedFragment = HomeFragment()
             }
 
             R.id.nav_search -> {
-                return@OnItemSelectedListener true
+                selectedFragment = SearchFragment()
             }
 
             R.id.nav_add_post -> {
-                return@OnItemSelectedListener true
+                selectedFragment = HomeFragment()
             }
 
             R.id.nav_notifications -> {
-                return@OnItemSelectedListener true
+                selectedFragment = NotificationsFragment()
             }
 
             R.id.nav_profile -> {
-                return@OnItemSelectedListener true
+                selectedFragment = ProfileFragment()
             }
         }
-
+        if (selectedFragment != null) {
+            supportFragmentManager.beginTransaction().replace(
+                R.id.fragment_container,
+                selectedFragment!!
+            ).commit()
+        }
         false
     }
 
@@ -48,7 +60,11 @@ class MainActivity : AppCompatActivity() {
         setContentView(binding.root)
 
         val navView: BottomNavigationView = binding.navView
-
         navView.setOnItemSelectedListener(onItemSelectedListener)
+
+        supportFragmentManager.beginTransaction().replace(
+            R.id.fragment_container,
+            HomeFragment()
+        ).commit()
     }
 }

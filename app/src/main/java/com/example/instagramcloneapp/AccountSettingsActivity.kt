70c1532d124f078b6a1678cf7b22ca4db7b8f999
Mohammed Shaat <mohammedshaat.it@gmail.com>
2023-08-30 15:01:39 +0300

    Commit 09

--- app/src/main/java/com/example/instagramcloneapp/AccountSettingsActivity.kt
+++ app/src/main/java/com/example/instagramcloneapp/AccountSettingsActivity.kt
@@ -1,11 +1,34 @@
 package com.example.instagramcloneapp
 
-import androidx.appcompat.app.AppCompatActivity
+import android.content.Intent
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import com.example.instagramcloneapp.databinding.ActivityAccountSettingsBinding
+import com.google.firebase.auth.FirebaseAuth
 
 class AccountSettingsActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityAccountSettingsBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_account_settings)
+
+        binding = ActivityAccountSettingsBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        val logoutBtn = binding.logoutBtn
+        logoutBtn.setOnClickListener {
+            logout()
+        }
+    }
+
+    private fun logout() {
+        val auth = FirebaseAuth.getInstance()
+        auth.signOut()
+
+        val intent = Intent(this, SignInActivity::class.java)
+        intent.flags = Intent.FLAG_ACTIVITY_CLEAR_TASK or Intent.FLAG_ACTIVITY_NEW_TASK
+        startActivity(intent)
+        finish()
     }
 }
--- app/src/main/java/com/example/instagramcloneapp/SignInActivity.kt
+++ app/src/main/java/com/example/instagramcloneapp/SignInActivity.kt
@@ -1,8 +1,10 @@
 package com.example.instagramcloneapp
 
+import android.app.ProgressDialog
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toast
 import com.example.instagramcloneapp.databinding.ActivitySignInBinding
 import com.google.firebase.auth.FirebaseAuth
 
@@ -21,6 +23,11 @@ class SignInActivity : AppCompatActivity() {
             val intent = Intent(this, SignUpActivity::class.java)
             startActivity(intent)
         }
+
+        val loginBtn = binding.loginBtn
+        loginBtn.setOnClickListener {
+            login()
+        }
     }
 
     override fun onStart() {
@@ -31,6 +38,48 @@ class SignInActivity : AppCompatActivity() {
             intent.flags = Intent.FLAG_ACTIVITY_CLEAR_TASK or Intent.FLAG_ACTIVITY_NEW_TASK
             startActivity(intent)
             finish()
+        }
+    }
+
+    private fun login() {
+        val email = binding.emailLogin.text.toString()
+        val password = binding.passwordLogin.text.toString()
+
+        when {
+            email.isBlank() -> {
+                Toast.makeText(this, "Email is required", Toast.LENGTH_LONG).show()
+            }
+
+            password.isBlank() -> {
+                Toast.makeText(this, "Password is required", Toast.LENGTH_LONG).show()
+            }
+
+            else -> {
+                val mAuth = FirebaseAuth.getInstance()
+                val progressDialog = ProgressDialog(this@SignInActivity).apply {
+                    setTitle("Signin")
+                    setMessage("Please wait")
+                    setCancelable(false)
+                    setCanceledOnTouchOutside(false)
+                }
+                progressDialog.show()
+
+                mAuth.signInWithEmailAndPassword(email, password)
+                    .addOnCompleteListener { task ->
+                        if (task.isSuccessful) {
+                            val intent = Intent(this, MainActivity::class.java)
+                            intent.flags =
+                                Intent.FLAG_ACTIVITY_CLEAR_TASK or Intent.FLAG_ACTIVITY_NEW_TASK
+                            startActivity(intent)
+                            finish()
+                        } else {
+                            val message = task.exception.toString()
+                            Toast.makeText(this, "Error: $message", Toast.LENGTH_LONG).show()
+                            mAuth.signOut()
+                            progressDialog.dismiss()
+                        }
+                    }
+            }
         }
     }
 }
